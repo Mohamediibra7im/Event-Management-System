f0213ffca0402f29322805d0b8685096526a1dd1
MarwanAshref <marwanashref836@gmail.com>
2025-05-15 19:58:18 +0300

    Delete src/EventManagementSystem.java

--- src/EventManagementSystem.java
+++
@@ -1,9 +0,0 @@
-import ui.LoginFrame;
-import javax.swing.SwingUtilities;
-
-public class EventManagementSystem {
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new LoginFrame());
-    }
-}
